fb1bc715a4aad61b2b9e373c2745d3ff4a158bee
Daniil <dpancov64@gmail.com>
2022-10-23 15:29:52 +0300

    Updated constants, deploy script

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -11,13 +11,12 @@ import {
 import { ethers, run } from 'hardhat'
 import { utils } from 'ethers'
 
+const { formatEther } = utils
+
 async function main() {
   const [deployer] = await ethers.getSigners()
   console.log('Deploying contracts with the account:', deployer.address)
-  console.log(
-    'Account balance:',
-    utils.formatEther(await deployer.getBalance())
-  )
+  console.log('Account balance:', formatEther(await deployer.getBalance()))
   const provider = ethers.provider
   const { chainId } = await provider.getNetwork()
   const chains = {
@@ -40,19 +39,18 @@ async function main() {
     SC_EXTERNAL_ERC721_POSTS_CONTRACT_ADDRESS,
   ]
   const contract = await Contract.deploy(targets)
+  const { address, setRelayHub, setTrustedForwarder, deployTransaction } =
+    contract
+  console.log(`Deploying to ${address} on ${chainName}...`)
   console.log(
     'Deploy tx gas price:',
-    utils.formatEther(contract.deployTransaction.gasPrice || 0)
+    formatEther(deployTransaction.gasPrice || 0)
   )
-  console.log(
-    'Deploy tx gas limit:',
-    utils.formatEther(contract.deployTransaction.gasLimit)
-  )
+  console.log('Deploy tx gas limit:', formatEther(deployTransaction.gasLimit))
   await contract.deployed()
-  const address = contract.address
   console.log('Setting relay hub and trusted forwarder...')
-  await contract.setRelayHub(GSN_RELAY_HUB_CONTRACT_ADDRESS)
-  await contract.setTrustedForwarder(GSN_FORWARDER_CONTRACT_ADDRESS)
+  await setRelayHub(GSN_RELAY_HUB_CONTRACT_ADDRESS)
+  await setTrustedForwarder(GSN_FORWARDER_CONTRACT_ADDRESS)
 
   console.log('Contract deployed to:', address)
   console.log('Wait for 1 minute to make sure blockchain is updated')
